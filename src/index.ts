4897ee93655b25be644e57ce3050fae4de8d6f62
concefly <concefly@foxmail.com>
2019-05-18 23:35:10 +0800

    0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('@@@', '1 ->', 1);
+export interface IConfig {
+  min: number;
+  max: number;
+}
+
+export interface IDep {
+  os: typeof import('os');
+}
+
+class BatchLimiter {
+  private config: IConfig = {
+    min: 1,
+    max: Infinity,
+  };
+
+  private dep: IDep = {
+    os: require('os'),
+  };
+
+  constructor(config?: Partial<IConfig>, dep?: Partial<IDep>) {
+    Object.assign(this.config, config);
+    Object.assign(this.dep, dep);
+
+    if (this.config.max < this.config.min) throw new Error('MAX mast be greater the MIN');
+  }
+
+  /** 内存负载 (0, 1] */
+  getMemoryLoad(): number {
+    const { os } = this.dep;
+
+    const free = os.freemem();
+    const total = os.totalmem();
+
+    return (total - free) / total;
+  }
+
+  getLimit(): number {
+    const { min, max } = this.config;
+
+    const memoryLoad = this.getMemoryLoad();
+
+    return Math.floor(min + (max - min) * (1 - memoryLoad));
+  }
+}
+
+export default BatchLimiter;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,49 @@
 import * as assert from 'assert';
+import BatchLimiter from '../src/index';
 
-describe('dry', () => {
-  it('dry', () => assert(1 === 1));
+describe('BatchLimiter', () => {
+  describe('memoryLoad', () => {
+    [
+      { freemem: 0, totalmem: 100, expect: 1 },
+      { freemem: 50, totalmem: 100, expect: 0.5 },
+      { freemem: 100, totalmem: 100, expect: 0 },
+    ].map(spec => {
+      it(`expect ${spec.expect}`, () => {
+        const limiter = new BatchLimiter(
+          {},
+          {
+            os: {
+              freemem: () => spec.freemem,
+              totalmem: () => spec.totalmem,
+            } as any,
+          }
+        );
+
+        assert.equal(limiter.getMemoryLoad(), spec.expect);
+      });
+    });
+  });
+
+  describe('getLimit', () => {
+    [
+      { min: 1, max: 10, memoryLoad: 0, expect: 10 },
+      { min: 1, max: 10, memoryLoad: 0.5, expect: 5 },
+      { min: 1, max: 10, memoryLoad: 1, expect: 1 },
+    ].map(spec => {
+      it(`expect ${spec.expect}`, () => {
+        class BatchLimiterTest extends BatchLimiter {
+          getMemoryLoad() {
+            return spec.memoryLoad;
+          }
+        }
+
+        const limiter = new BatchLimiterTest({
+          min: spec.min,
+          max: spec.max,
+        });
+
+        assert.equal(limiter.getLimit(), spec.expect);
+      });
+    });
+  });
 });
